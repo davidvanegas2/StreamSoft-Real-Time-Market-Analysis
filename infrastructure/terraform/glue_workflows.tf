f21f04b4ce5b5d1f19d1f11b4e18332586893a81
David Felipe Vanegas Ramirez <david@loka.com>
2024-03-29 14:07:19 -0500

    feat: Add Infra to run Glue Workflows

---
+++ infrastructure/terraform/glue_workflows.tf
@@ -0,0 +1,42 @@
+resource "aws_glue_workflow" "workflow" {
+  name = "workflow_${var.lakehouse_name}"
+}
+
+resource "aws_glue_trigger" "start_streaming_job_trigger" {
+  name              = "start_streaming_job_trigger_${var.lakehouse_name}"
+  type              = "ON_DEMAND"
+  start_on_creation = true
+
+  actions {
+    job_name = aws_glue_job.streaming_job.name
+  }
+}
+
+resource "aws_glue_trigger" "start_workflow_trigger" {
+  name              = "start_workflow_trigger_${var.lakehouse_name}"
+  type              = "SCHEDULED"
+  schedule          = "cron(*/15 * * * *)"
+  start_on_creation = false
+  workflow_name     = aws_glue_workflow.workflow.name
+
+  actions {
+    job_name = aws_glue_job.silver.name
+  }
+}
+
+resource "aws_glue_trigger" "end_workflow_trigger" {
+  name          = "end_workflow_trigger_${var.lakehouse_name}"
+  type          = "CONDITIONAL"
+  workflow_name = aws_glue_workflow.workflow.name
+
+  predicate {
+    conditions {
+      job_name = aws_glue_job.silver.name
+      state    = "SUCCEEDED"
+    }
+  }
+
+  actions {
+    job_name = aws_glue_job.gold.name
+  }
+}
